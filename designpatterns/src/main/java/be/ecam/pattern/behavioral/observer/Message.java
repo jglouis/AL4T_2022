2960f62e7e1c43c92779882f866ba5c20114fc38
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2022-10-20 17:23:17 +0200

    Add reactive programming sample

--- designpatterns/src/main/java/be/ecam/pattern/behavioral/observer/Message.java
+++ designpatterns/src/main/java/be/ecam/pattern/behavioral/observer/Message.java
@@ -12,6 +12,14 @@ public class Message {
         this.message = message;
     }
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
     @Override
     public String toString() {
         return String.format("%s: %s", author, message);
