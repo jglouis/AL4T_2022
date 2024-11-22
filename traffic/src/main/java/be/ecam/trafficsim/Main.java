65a22728e462fae791cb38a15ea1ed10ef6d2957
NJurquet <nicolor01@hotmail.com>
2024-11-22 17:39:57 +0100

    Added `NullPointerException` catch for sounds Map

--- traffic/src/main/java/be/ecam/trafficsim/Main.java
+++ traffic/src/main/java/be/ecam/trafficsim/Main.java
@@ -15,12 +15,20 @@ public class Main {
         jF.setSize(1366, 750);
         jF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jF.setVisible(true);
-        Map<String, InputStream> sounds = Map.of(
-                "traffic", Objects.requireNonNull(Main.class.getResourceAsStream("/Traffic Sounds - Free Sound Effects - Traffic Sound Clips - Sound Bites.wav")),
-                "drift", Objects.requireNonNull(Main.class.getResourceAsStream("/drift.wav"))
-        );
+
+        Map<String, InputStream> sounds = null;
+        try {
+            sounds = Map.of(
+                    "traffic", Objects.requireNonNull(Main.class.getResourceAsStream("/Traffic Sounds - Free Sound Effects - Traffic Sound Clips - Sound Bites.wav")),
+                    "drift", Objects.requireNonNull(Main.class.getResourceAsStream(""))
+            );
+        } catch (NullPointerException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
         final SoundManager soundManager = new SoundManager(sounds);
         soundManager.play();
+
         jF.addWindowListener(new WindowListener() {
             public void windowOpened(WindowEvent e) {
             }
