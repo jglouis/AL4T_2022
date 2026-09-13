2c94d04502610bb431b7e7f4e3243b52cfbdc689
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2026-09-13 17:45:10 +0200

    Add junit-platform-launcher where necessary

--- traffic/build.gradle.kts
+++ traffic/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation ("org.jetbrains:annotations:16.0.2")
 }
 application {
